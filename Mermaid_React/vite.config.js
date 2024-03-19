2a42fbe2016607d13302583fd4c37b5bde24d718
JohnnytheShark <orellanajonathan7@gmail.com>
2024-03-19 12:45:19 -0400

    Deployment Updates

--- Mermaid_React/vite.config.js
+++ Mermaid_React/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React/Mermaid_React"
+  base: "/React_Mermaid/"
 })
