c8afbbf3be790391f5d64a97ac0843b8f4e14076
JohnnytheShark <orellanajonathan7@gmail.com>
2024-03-19 11:53:16 -0400

    Added functionality of the example

--- Mermaid_React/src/App.jsx
+++ Mermaid_React/src/App.jsx
@@ -3,16 +3,52 @@ import Mermaid from './components/Mermaid';
 
 function App() {
   const chartDefinition = `
-  graph TD;
-  A[]-->B;
-  A-->C;
-  B-->D;
-  C-->D;
+  ---
+  title: Entity Relationship Diagram Example E-commerce
+  ---
+  erDiagram
+  CUSTOMER }|--o{ ORDERS : places
+    CUSTOMER{
+      int id PK
+      varchar name
+      varchar email
+      date creation_date
+    }
+
+    ORDERS{
+      int id PK
+      date order_date
+      varchar status
+    }
+    ORDERS ||--|{ PRODUCTS : have
+    PRODUCTS{
+      int id PK
+      varchar product
+      varchar product_description
+      int amount
+      int inventory
+      date updatedDate
+    }
+    CUSTOMER }|--o{ SHIPPINGADDRESS : have
+    SHIPPINGADDRESS{
+      int id PK
+      int user_id FK
+      varchar address_line1
+      varchar address_line2
+      varchar city
+      int postal_code
+      varchar country
+      varchar phone
+    }
+
+
 `;
   return (
-    <>
+    <main>
       <Mermaid chart={chartDefinition} id="Diagram Example"/>
-    </>
+      <p>Want to use Mermaid.js with your React projects? Check out how I did it here:</p>
+      <a href="https://github.com/JohnnytheShark/React/tree/main/Mermaid_React">Github Repository</a>
+    </main>
   )
 }
 
--- Mermaid_React/src/components/Mermaid.jsx
+++ Mermaid_React/src/components/Mermaid.jsx
@@ -1,14 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import mermaid from 'mermaid';
 
+
 const Mermaid = ({ chart, id }) => {
+ const containerRef = useRef(null);
+
  useEffect(() => {
-    mermaid.initialize({ startOnLoad: true });
-    mermaid.init(undefined, document.getElementById(id));
+    mermaid.initialize({
+      startOnLoad:true,
+      theme: 'base',
+      erDiagram: {
+         useMaxWidth: false,
+        chartWidth: 1080,
+        chartHeight: 1920,
+        minEntityWidth: 100, // Minimum width for entities
+        minEntityHeight: 50, // Minimum height for entities
+        entityPadding: 20 // Padding around entities
+      }
+    });
+
+    const renderMermaid = () => {
+      mermaid.render(id, chart, (svgCode) => {
+        if (containerRef.current) {
+          containerRef.current.innerHTML = svgCode;
+        }
+      });
+    };
+
+    renderMermaid();
  }, [chart, id]);
 
  return (
-    <div className="mermaid" id={id}>
+    <div className="mermaid" id={id} ref={containerRef}>
       {chart}
     </div>
  );
